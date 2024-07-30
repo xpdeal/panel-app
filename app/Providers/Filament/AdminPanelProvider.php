61a85fb00b2926f3888f9c8bc038df8997d4fbf2
Jordan <brasil@xpdeal.com>
2024-07-30 07:25:49 -0300

    bkp

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->viteTheme('resources/css/filament/admin/theme.css')
-            ->plugin(\TomatoPHP\FilamentPlugins\FilamentPluginsPlugin::make())
+            //->plugin(\TomatoPHP\FilamentPlugins\FilamentPluginsPlugin::make())
             ->login()
             ->colors([
                 'primary' => Color::Amber,
